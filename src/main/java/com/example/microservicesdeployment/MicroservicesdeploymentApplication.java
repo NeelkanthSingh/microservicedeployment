f08d09e89e9cdaa6cde76248cfe714728b493deb
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-05-08 17:24:12 +0530

    first commit

---
+++ src/main/java/com/example/microservicesdeployment/MicroservicesdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicesdeployment/MicroservicesdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
